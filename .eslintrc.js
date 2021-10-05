18bcc45876362121cdff873572e5961f4975691c
Guillermo Croppi <guillermocroppi@gmail.com>
2021-10-05 19:25:04 -0300

    Updated vue-cli

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,18 +1,17 @@
 module.exports = {
   root: true,
   env: {
-    node: true,
+    node: true
   },
-  extends: [
+  'extends': [
     'plugin:vue/essential',
-    '@vue/airbnb',
+    'eslint:recommended'
   ],
-  rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'max-len': 'off',
-  },
   parserOptions: {
-    parser: 'babel-eslint',
+    parser: 'babel-eslint'
   },
-};
+  rules: {
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
+  }
+}
--- vue.config.js
+++ vue.config.js
@@ -2,10 +2,9 @@ module.exports = {
   css: {
     loaderOptions: {
       sass: {
-        data: `
-          @import "~bulma/sass/utilities/initial-variables.sass";
-          @import "~bulma/sass/utilities/mixins.sass";
-        `,
+        prependData: `@import "~bulma/sass/utilities/initial-variables.sass";
+        @import "~bulma/sass/utilities/mixins.sass";`
+       
       },
     },
   },
